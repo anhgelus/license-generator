1a1f3a43170d6b9b38d3a51ddd8754cde50c5ff5
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-05-08 13:44:51 +0200

    feat(config): use XDG_CONFIG_HOME for config folder
    #8

--- src/config/static.go
+++ src/config/static.go
@@ -2,15 +2,16 @@ package config
 
 import (
 	"errors"
-	"github.com/anhgelus/license-generator/src/utils"
 	"os"
 )
 
 func ImportStaticConfig() ([]*LicenseConfig, error) {
-	dirPath, err := os.UserHomeDir()
-	utils.HandleError(err)
-	configPath := dirPath + "/.config/license-generator/"
-	err = os.Mkdir(configPath, 777)
+	dirPath := os.Getenv("XDG_CONFIG_HOME")
+	if dirPath == "" {
+		dirPath = os.Getenv("HOME") + "/.config"
+	}
+	configPath := dirPath + "/license-generator/"
+	err := os.Mkdir(configPath, 777)
 	if err != nil && !errors.Is(err, os.ErrExist) {
 		return nil, err
 	}
