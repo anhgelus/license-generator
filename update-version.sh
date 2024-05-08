e401c93b9f474cb943c391a9c8eb41e0e51c599e
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-05-08 15:14:45 +0200

    build(script): put version name in commit of PKGBUILD update

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sed -i "s/$1/$2/g" PKGBUILD
-git commit -am "build(arch): update pkgbuild"
+git commit -am "build(arch): update pkgbuild for $2"
 git tag $2
 git push
 git push origin $2
